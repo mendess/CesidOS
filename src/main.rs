f3f83cd4d8a2a3b4f19e53756abfca7dd4d11490
Mendess2526 <pedro.mendes.26@gmail.com>
2019-07-31 17:32:16 +0100

    Add vga buffer wrapper

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,25 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
+
+use vga_buffer::Writer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static CESIDOS: &[u8] = b"CesidOS";
-static HELLO: &[u8] = b"Hello world!";
+static CESIDOS: &str = "CesidOS";
+static HELLO: &str = "Hello world!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in CESIDOS.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xc;
-        }
-    }
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2 + 160) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1 + 160) = 0xb;
-        }
-    }
+    let mut writer = Writer::new();
+    writer.write_str(CESIDOS);
+    writer.write_byte(b'\n');
+    writer.write_str(HELLO);
     loop {}
 }
